abb15fcc39dc6c6542ddd04ac3ddc31385c68628
Wilson Aguilar <52500170+waguilars@users.noreply.github.com>
2022-03-02 12:48:44 -0500

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['standard', 'standard-jsx'],
+  plugins: ['simple-import-sort', 'import'],
+  rules: {
+    'import/first': 1,
+    'import/newline-after-import': 1,
+    'import/no-duplicates': 1,
+    semi: [1, 'always'],
+    'simple-import-sort/imports': 1,
+    'simple-import-sort/exports': 1,
+  },
+};
